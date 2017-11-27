ad9d8aa4caf25c717464952b4760c07a25ac0ffb
Ian Macato <imacato@stanford.edu>
2017-11-27 12:51:14 -0800

    working clickthrough

--- Otter/ViewController.swift
+++ Otter/ViewController.swift
@@ -19,6 +19,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 0
+    }
+    
+    
 
 
 }
