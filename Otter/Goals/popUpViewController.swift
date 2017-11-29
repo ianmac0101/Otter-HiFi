20ee25559aff88af6187dd77747f1c9fe4b7b6b0
Gustavo Chavez <gustavochavez01@gmail.com>
2017-11-29 11:46:42 -0800

    binary files changed by Ian

--- Otter/Goals/popUpViewController.swift
+++ Otter/Goals/popUpViewController.swift
@@ -13,7 +13,7 @@ class popUpViewController: UIViewController {
     @IBOutlet weak var confirmationText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        confirmationText.text = "Created " + (goalsList.last?.activity)! + "activity!"
+        confirmationText.text = "Created " + (goalsList.last?.activity)! + " activity!"
         // Do any additional setup after loading the view.
     }
 
@@ -21,16 +21,4 @@ class popUpViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
