8be72e83ceb6382a5a4347f320b641767f0c1061
Ian Macato <imacato@stanford.edu>
2017-11-30 12:51:34 -0800

    timer update

--- Otter/practicesTimerViewController.swift
+++ Otter/practicesTimerViewController.swift
@@ -6,4 +6,89 @@
 //  Copyright © 2017 Otter. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class practicesTimerViewController: UIViewController {
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK: - IBOutlets
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+
+    
+    var seconds = 60
+    var timer = Timer()
+    
+    var isTimerRunning = false
+    var resumeTapped = false
+    
+    //MARK: - IBActions
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        if isTimerRunning == false {
+            runTimer()
+            self.startButton.isEnabled = false
+        }
+    }
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+        isTimerRunning = true
+        pauseButton.isEnabled = true
+    }
+    
+    @IBAction func pauseButtonTapped(_ sender: UIButton) {
+        if self.resumeTapped == false {
+            timer.invalidate()
+            isTimerRunning = false
+            self.resumeTapped = true
+            self.pauseButton.setTitle("Resume",for: .normal)
+        } else {
+            runTimer()
+            self.resumeTapped = false
+            isTimerRunning = true
+            self.pauseButton.setTitle("Pause",for: .normal)
+        }
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        seconds = 60
+        timerLabel.text = timeString(time: TimeInterval(seconds))
+        isTimerRunning = false
+        pauseButton.isEnabled = false
+        startButton.isEnabled = true
+    }
+    
+    
+    func updateTimer() {
+        if seconds < 1 {
+            timer.invalidate()
+            //Send alert to indicate time's up.
+        } else {
+            seconds -= 1
+            timerLabel.text = timeString(time: TimeInterval(seconds))
+            timerLabel.text = String(seconds)
+            //            labelButton.setTitle(timeString(time: TimeInterval(seconds)), for: UIControlState.normal)
+        }
+    }
+    
+    func timeString(time:TimeInterval) -> String {
+        let hours = Int(time) / 3600
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
+    }
+    
+    //MARK: - Super Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pauseButton.isEnabled = false
+    }
+}
+
+
